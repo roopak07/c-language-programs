0756d2becf9aec74a564e5b37684a4bf0a8e11f0
Roopak <h.rupak61@gmail.com>
2020-02-15 16:17:43 +0530

    Create decimal_to_binary_bitwise_operator.c

---
+++ decimal_to_binary_bitwise_operator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define bit sizeof(int)*8 // for 32 bit processor
+int b[bit];
+int main()
+{
+	int val;
+    printf("Converting decimal into binary using bitwise operator\n");
+	printf("\nEnter a value: \n");
+	scanf("%d",&val);
+	printf("\nbit=%d",bit);
+    for(int i=bit-1;i>=0;i--)
+	{
+		b[i] = val&1;
+		val=val>>1;
+	}
+	printf("\n Binary number is:");
+	for(int i=0;i<bit;i++)
+	{
+		printf("%d",b[i]);
+	}
+    return 0;
+}
