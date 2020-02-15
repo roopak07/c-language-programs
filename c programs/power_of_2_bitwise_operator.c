4d7775731d3b8f10a70c416d3519078ed79747f5
Roopak <h.rupak61@gmail.com>
2020-02-15 15:45:56 +0530

    Create power_of_2_bitwise_operator.c

---
+++ c programs/power_of_2_bitwise_operator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#define bit sizeof(int) // for 32 bit processor
+int b[bit];
+int main()
+{
+	int val,pow=0;
+    printf("Finding weather given number is power of 2 or not \n");
+	printf("Enter a value: \n");
+	scanf("%d",&val);
+    for(int i=1;i<=bit;i++)
+	{
+		if(val&1 == 1)
+			pow++;
+		val=val>>1;
+	}
+	if(pow==1)
+		printf("Given numbber is the power of 2 \n");
+	else
+		printf("Given number is not the power of 2 \n");
+    return 0;
+}
