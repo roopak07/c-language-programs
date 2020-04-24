1a46e3ef19205f27701e0e6ef7d28d1deef8a99d
Roopak <h.rupak61@gmail.com>
2020-04-24 18:25:55 +0530

    Create cmdline.c

---
+++ cmdline.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void main(int argc,char *argv[])
+{
+ printf("Argc = %d\n",argc); // argc will give no of inputs to program
+ printf("Argv = %s",argv[0]); // by default argv[0] will store file name, from argv[1] it will store inputs to the program
+}
