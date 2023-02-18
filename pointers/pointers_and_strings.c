3436a7a35a3be277d9dac3e0a83b46442421b00a
h.rupak61@gamil.com <roopak07>
2023-02-18 17:04:18 +0530

    Included some basic concepts of strings

---
+++ pointers/pointers_and_strings.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	char ch[] = "cooding";
+	printf("%s\n", ch);
+	printf("sizeof string = %d , length of the string = %d\n", sizeof(ch), strlen(ch));
+}
