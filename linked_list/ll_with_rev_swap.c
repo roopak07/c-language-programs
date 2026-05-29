26a3b47fe45d9f2d930898a86e652453ac295899
Roopak <h.rupak61@gmail.com>
2026-05-29 12:27:27 +0530

    Add linked list implementation with reverse functionality

---
+++ linked_list/ll_with_rev_swap.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<stdint.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *root=NULL;
+
+int insert();
+int delete();
+int length();
+int display();
+int reverse();
+
+int main()
+{
+    uint32_t ch=0;
+    while(1)
+    {
+        printf("Select from below:\n");
+        printf("1.Insert\n");
+        printf("2.Delete\n");
+        printf("3.Length\n");
+        printf("4.Display\n");
+        printf("5.Reverse\n");
+        printf("9.exit\n");
+        printf("Enter your choise:\n");
+        scanf("%d", &ch);
+        switch(ch)
+        {
+            case 1: insert();
+                    break;
+            case 2: delete();
+                    break;
+            case 3: length();
+                    break;
+            case 4: display();
+                    break;
+            case 5: reverse();
+                    break;
+            default: 
+                    return 0;
+        }
+    }
+    return 0;
+}
+
+int insert()
+{
+    struct node *temp=NULL;
+    uint32_t data=0;
+    temp=(struct node*)(malloc(sizeof(struct node)));
+    printf("Enter data to insert in the node:");
+    scanf("%d", &data);
+    temp->data = data;
+    if(root==NULL)
+        root=temp;
+    else
+    {
+        struct node *p=root;
+        while(p->link!=NULL)
+            p=p->link;
+        p->link = temp;
+    }
+    
+    return 0;
+}
+
+int delete()
+{
+    struct node *temp=root, *current=root;
+    int len=0, del_ch=0;
+    len = length();
+    printf("Enter the node to delete:");
+    scanf("%d", &del_ch);
+    if(len == 0)
+        printf("There are no nodes to delete\n");
+    else if(del_ch > len)
+        printf("Entered choise is outoff range\n");
+    else 
+    {
+        for(int i=0; i<(del_ch-1); i++)
+            current=current->link;
+        temp = current->link; // point to next node , so temp will have next node address
+        // Copy the next node data into present node and free the temp node
+        current->data=temp->data;
+        current->link = temp->link;
+        // Delet the next node as we copied the next node into current node
+        temp->link = NULL;
+        free(temp);
+    }
+    return 0;
+}
+
+int length()
+{
+    uint32_t len=0;
+    struct node *p=root;
+    while(p!=NULL)
+    {
+        len++;
+        p=p->link;
+    }
+    printf("No:of nodes:%d\n", len);
+    return len;
+}
+
+int display()
+{
+    struct node *p=root;
+    while(p!=NULL)
+    {
+        printf("%d | 0x%x   ", p->data, p->link);
+        p=p->link;
+    }
+    printf("\n");
+    return 0;
+}
+
+int reverse()
+{
+    int len=0;
+    struct node *current=root, *next=root, *prev=NULL;
+    len=length();
+    if(len < 2)
+        printf("There are no nodes to delete\n");
+    while(current!=NULL)
+    {
+        next=current->link;
+        current->link = prev;
+        prev=current;
+        current=next;
+    }
+    root=prev;
+}
